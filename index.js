28c7578c289c3fabdb6eb49a7adb402bc85d33c3
reefgb96 <reef.goldberg904@gmail.com>
2022-07-09 13:12:08 +0300

    inisialze-rep

---
+++ index.js
@@ -0,0 +1,22 @@
+let outputScreen = document.getElementById('output-screen');
+
+function display(num) {
+    outputScreen.value += num;
+}
+
+function Calculate() {
+    try{
+        outputScreen.value = eval(outputScreen.value);
+    }
+    catch(err){ 
+        alert('Cant Calculate! typing error.')
+        outputScreen.value = '';
+    }
+}
+
+function Clear() {
+    outputScreen.value = '';
+}
+function del() {
+    outputScreen.value = outputScreen.value.slice(0, -1);
+}
